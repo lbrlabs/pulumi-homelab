476a3ad9228d51927b9236e131d8dbe5a4b09075
Lee Briggs <lbriggs@apptio.com>
2020-03-13 13:21:32 -0700

    Reorg repo
    - Move into language specific directory
    - Use new stack name
    - Add metallb and ingress controller

---
+++ typescript/cluster/index.ts
@@ -0,0 +1,90 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as rke from "@jaxxstorm/pulumi-rke";
+import * as k8s from "@pulumi/kubernetes";
+import * as fs from "fs";
+
+// Get the config from the stack
+let config = new pulumi.Config()
+
+// Cluster name is configurable
+const clusterName = config.require("name")
+const metallbChartVersion = config.require("metallbChartVersion")
+
+// Define an interface to read the node config
+// We want the nodes to be configurable
+interface Nodes {
+    [index: number]: {
+        address: string;
+        roles: string[];
+        user: string;
+    }
+}
+
+// Grab the node config
+let nodes = config.requireObject<Nodes>("nodes");
+
+
+// Create an RKE cluster!
+const cluster = new rke.Cluster(clusterName, {
+    clusterName: clusterName,
+    ignoreDockerVersion: false,
+    sshAgentAuth: true,
+    nodes: nodes as any[], // cast the interface to an array
+    ingress: { provider: "none" },
+});
+
+// export the kubeconfig for the cluster
+export const kubeconfig = cluster.kubeConfigYaml
+
+// Set up metallb
+const provider = new k8s.Provider(clusterName, { kubeconfig });
+
+const metallbConfig = new k8s.core.v1.ConfigMap("metallb-config", {
+    metadata: { namespace: "kube-system" },
+    data: { "config": fs.readFileSync("metallb-config.yaml").toString() },
+});
+const metallbConfigName = metallbConfig.metadata.apply(m => m.name);
+
+const metallb = new k8s.helm.v2.Chart("metallb",
+    {
+        namespace: "kube-system",
+        chart: "metallb",
+        version: metallbChartVersion,
+        fetchOpts: { repo: "https://kubernetes-charts.storage.googleapis.com/" },
+        values: { existingConfigMap: metallbConfigName },
+    },
+    { providers: { kubernetes: provider } },
+);
+
+// set up nginx-ingress
+
+const nginx = new k8s.helm.v2.Chart("nginx-ingress",
+    {
+        namespace: "kube-system",
+        chart: "nginx-ingress",
+        version: "1.33.5",
+        fetchOpts: { repo: "https://kubernetes-charts.storage.googleapis.com/" },
+        values: {
+            stats: {
+                enabled: true,
+            },
+            service: {
+                type: "LoadBalancer",
+            },
+            publishService: {
+                enabled: true,
+            },
+            metrics: {
+                enabled: true,
+            },
+            extraArgs: {
+                "sort-backends": true,
+            }
+        }
+    },
+    { providers: { kubernetes: provider } },
+)
+
+
+
+
