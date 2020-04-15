421fd94dad9da1bd3d8cb0653f1c310034013317
Lee Briggs <lee@leebriggs.co.uk>
2020-04-14 18:22:34 -0700

    Add drone

---
+++ drone/index.ts
@@ -0,0 +1,77 @@
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+
+// Get the config from the stack
+let config = new pulumi.Config()
+const stack = pulumi.getStack()
+const stackRef = `jaxxstorm/cluster/${stack}`;
+
+// Get stack references
+const cluster = new pulumi.StackReference(stackRef); // # FIXME: make configurable
+const provider = new k8s.Provider("k8s", { kubeconfig: cluster.getOutput("kubeConfig") });
+
+// Set configuration values
+const ns = config.require("namespace")
+const rpcSecret = config.require("rpcSecret")
+const hostUri = config.require("hostUri")
+const httpProtocol = config.get("httpProtocol") || "https";
+const githubClientId = config.require("githubClientId")
+const githubClientSecret = config.require("githubClientSecret")
+
+const namespace = new k8s.core.v1.Namespace("ns", {
+    metadata: {
+        name: ns,
+    }
+}, { provider: provider });
+
+const drone = new k8s.helm.v2.Chart("drone",
+     {
+         namespace: namespace.metadata.name,
+         chart: "drone",
+         version: "0.1.5",
+         fetchOpts: { repo: "https://charts.drone.io" },
+         values: { 
+             env: {
+                DRONE_SERVER_HOST: hostUri,
+                DRONE_SERVER_PROTO: httpProtocol,
+                DRONE_RPC_SECRET: rpcSecret,
+                DRONE_GITHUB_CLIENT_ID: githubClientId,
+                DRONE_GITHUB_CLIENT_SECRET: githubClientSecret,
+             },
+             ingress: {
+                 enabled: true,
+                 annotations: {
+                    "kubernetes.io/ingress.class": "nginx"
+                 },
+                 hosts: [
+                     { host: hostUri, paths: [ "/" ] }
+                 ],
+                 tls: [
+                    { hosts: [ hostUri ] }
+                 ]
+             }
+         },
+     },
+     { providers: { kubernetes: provider } },
+ );
+
+ const droneRunner = new k8s.helm.v2.Chart("drone-runner",
+     {
+        namespace: namespace.metadata.name,
+        chart: "drone-runner-kube",
+        version: "0.1.2",
+        fetchOpts: { repo: "https://charts.drone.io" },
+        values: {
+            env: {
+                DRONE_RPC_SECRET: rpcSecret,
+                DRONE_RPC_HOST: hostUri,
+                DRONE_RPC_PROTO: httpProtocol,
+                DRONE_NAMESPACE_DEFAULT: namespace.metadata.name,
+                DRONE_RPC_SKIP_VERIFY: true,
+            }
+        }
+     },
+     { providers: { kubernetes: provider } },
+ );
+
+
